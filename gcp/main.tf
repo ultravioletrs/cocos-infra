c9267a0be7eba26f92120102768dbafaf85c935d
Sammy Oina <sammyoina@gmail.com>
2025-01-16 19:09:50 +0300

    Add GCP Terraform configurations for improved VM setup and security features
    Signed-off-by: Sammy Oina <sammyoina@gmail.com>

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,15 +1,20 @@
 locals {
-  cloud_init_config = filebase64("${path.module}/../cloud-init/base.yaml")
+  cloud_init_config = filebase64("${path.module}/../cloud-init/base.yml")
 }
 
 resource "google_compute_instance" "confidential" {
   name         = var.vm_name
   machine_type = "n2d-standard-2"
+  min_cpu_platform = "AMD Milan"
   zone         = "${var.region}-a"
+
+  scheduling {
+    on_host_maintenance = "TERMINATE"  
+  }
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20250112"
     }
   }
 
@@ -20,9 +25,17 @@ resource "google_compute_instance" "confidential" {
 
   confidential_instance_config {
     enable_confidential_compute = true
+    confidential_instance_type = "SEV_SNP"
   }
 
   metadata = {
     user-data = local.cloud_init_config
   }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+  }
+  
 }
